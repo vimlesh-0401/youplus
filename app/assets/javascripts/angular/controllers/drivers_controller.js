0496bc9479bb3a8f9585629cd0b4b50bb9dd843d
Vimlesh Patel <vimlesh.0401@gmail.com>
2019-04-08 06:22:59 +0000

    updated gem files

--- app/assets/javascripts/angular/controllers/drivers_controller.js
+++ app/assets/javascripts/angular/controllers/drivers_controller.js
@@ -4,7 +4,6 @@
   app.controller('DriversController', ["$scope", "$http", function($scope, $http) {
   	$scope.drivers = [];
   	$scope.requests = {waiting: [], ongoing: [], complete: []};
-  	$scope.fiveMin = (1000*5*60);
   	
   	$scope.onlyDrivers = function(){
   	  $http.get('/drivers.json', {})
